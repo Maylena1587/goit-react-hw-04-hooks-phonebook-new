05d6e95e30d1ccea9397409e433a7f22c4c9f382
Maylena1587 <65168939+Maylena1587@users.noreply.github.com>
2021-01-31 22:13:25 +0200

    вносит правки

---
+++ src/Components/App/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { v4 as uniqueId } from 'uuid';
+import Container from '../Container';
+import ContactList from '../ContactList';
+import ContactForm from '../ContactForm';
+import Section from '../Section';
+import Filter from '../Filter';
+import useLocalStorage from '../../hooks/customHooks';
+
+function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (name, number) => {
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase(),
+      )
+    ) {
+      alert(`${name} is already in Phonebook`);
+      return;
+    }
+
+    const newContact = {
+      id: uniqueId(),
+      name,
+      number,
+    };
+
+    setContacts(prevState => {
+      return [...prevState, newContact];
+    });
+  };
+
+  const deleteContact = e => {
+    const deletedId = e.currentTarget.dataset.id;
+
+    setContacts(prevState => {
+      return prevState.filter(contact => contact.id !== deletedId);
+    });
+
+    e.currentTarget.blur();
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase().trim();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm getFormData={addContact} />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length ? (
+          <>
+            <Filter
+              value={filter}
+              onChange={e => setFilter(e.currentTarget.value)}
+            />
+            {filter.trim() ? (
+              <ContactList
+                contacts={getFilteredContacts()}
+                deleteHandler={deleteContact}
+              />
+            ) : (
+              <ContactList contacts={contacts} deleteHandler={deleteContact} />
+            )}
+          </>
+        ) : (
+          <div>Oops. no contacts here! Let's add some data!</div>
+        )}
+      </Section>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/ContactList/ContactList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Contact from '../Contact/Contact';
+import styles from './ContactList.module.css';
+
+function ContactList({ contacts, deleteHandler }) {
+  return (
+    <ul className={styles.contactList}>
+      {contacts.map(contact =>
+        Contact({
+          id: contact.id,
+          name: contact.name,
+          phone: contact.number,
+          deleteHandler,
+        }),
+      )}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default ContactList;
